dda8930d50d81c056fba809cc188152ba8884adf
William Emfinger <waemfinger@gmail.com>
2023-07-12 14:11:27 -0500

    fix: added missing files.

---
+++ main/main.cpp
@@ -0,0 +1,39 @@
+#include <chrono>
+#include <thread>
+
+#include "logger.hpp"
+#include "task.hpp"
+
+using namespace std::chrono_literals;
+
+extern "C" void app_main(void) {
+  static auto start = std::chrono::high_resolution_clock::now();
+  static auto elapsed = [&]() {
+    auto now = std::chrono::high_resolution_clock::now();
+    return std::chrono::duration<float>(now - start).count();
+  };
+
+  espp::Logger logger({.tag = "Template", .level = espp::Logger::Verbosity::DEBUG});
+
+  logger.info("Bootup");
+
+  // make a simple task that prints "Hello World!" every second
+  espp::Task task({
+      .name = "Hello World",
+      .callback = [&](auto &m, auto &cv) -> bool {
+        logger.debug("[{:.3f}] Hello from the task!", elapsed());
+        std::unique_lock<std::mutex> lock(m);
+        cv.wait_for(lock, 1s);
+        // we don't want to stop the task, so return false
+        return false;
+      },
+      .stack_size_bytes = 4096,
+    });
+  task.start();
+
+  // also print in the main thread
+  while (true) {
+    logger.debug("[{:.3f}] Hello World!", elapsed());
+    std::this_thread::sleep_for(1s);
+  }
+}
